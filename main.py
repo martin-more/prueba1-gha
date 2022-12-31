2e9293ed0158341ffe729d81c435f24749e5749f
Martin More Panta <martinmore52@gmail.com>
2022-12-30 19:13:27 -0500

    test sensibility data

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def print_hi(name):
     # assert name == "bad_name", f"name should be More!!"  # PASS
-    x = 10
-    print(eval("x * 10"))
+    # x = 10
+    # print(eval("x * 10"))  # PASS
+    username = "user"
+    password = "admin123456"
+    url = "https://example.com/accounts/details?id=123&access_key=abcdefg&access_secret=opensecret"
+    print(username, password)
+    print(url)
     print(f'Hi, {name}')
 
 
