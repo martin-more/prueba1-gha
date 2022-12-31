74f21d728124fbeb2e6e4b5b6b194490aea8ad25
Martin More Panta <martinmore52@gmail.com>
2022-12-30 19:36:34 -0500

    fix sensitive logging

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ def print_hi(name):
     # assert name == "bad_name", f"name should be More!!"  # PASS
     # x = 10
     # print(eval("x * 10"))  # PASS
-    username = "user"
-    password = "admin123456"
-    url = "https://example.com/accounts/details?id=123&access_key=abcdefg&access_secret=opensecret"
-    print(username, password)
-    print(url)
+    # username = "user"           # FAIL
+    # password = "admin123456"    # FAIL
+    # print(username, password)
     print(f'Hi, {name}')
 
 
